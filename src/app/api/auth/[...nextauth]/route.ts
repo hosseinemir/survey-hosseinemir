826b197572573934ee847ab4bda46aac21ab51d5
hosseinemir <hosseinemir79@gmail.com>
2024-02-07 17:49:47 +0330

    back to last stage and undo auth jobs and start to do answer survey part

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,34 @@
 import User from "@/models/User";
 import { verifyPassword } from "@/utils/auth";
 import ConnectDB from "@/utils/ConnectDB";
+import { error } from "console";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+// export const authOptions = {
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [],
-};
-export default NextAuth(authOptions);
+//   providers: [CredentialsProvider({
+//     async authorize(credentials){
+//         const {email , password} = credentials;
+
+//         try{
+//             await ConnectDB()
+
+//         }catch(err){
+//             throw new Error("مشکلی در سمت سرور پیش امده است")
+//         }
+//         if(!email || !password){
+//             throw new Error("اطلاعات درست را وارد کنید ")
+//         }
+//         const user = await User.findOne({email})
+//         if (!user){
+//             throw new Error("ابتدا حساب کاربری ایجاد کنید")
+//         }
+//         const isvalid = await verifyPassword(password,user.password)
+//         if(!isvalid){
+//             throw new Error("ایمیل و یا پسورد اشتباه است")
+//         }
+//         return {email}
+//     }
+//   })],
+// };
+// export default NextAuth(authOptions);
--- src/components/template/AnswerSurveyT.tsx
+++ src/components/template/AnswerSurveyT.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 
 const surveyforexapmle = {
@@ -10,6 +10,11 @@ const surveyforexapmle = {
   questions: [
     { title: "1", img: "", answer: ["1", "2", "3", "4"] },
     { title: "2", img: "", answer: ["1", "2", "3", "4"] },
+    { title: "3", img: "", answer: ["1", "2", "3", "4"] },
+    { title: "4", img: "", answer: ["1", "2", "3", "4"] },
+    { title: "5", img: "", answer: ["1", "2", "3", "4"] },
+    { title: "6", img: "", answer: ["1", "2", "3", "4"] },
+    { title: "7", img: "", answer: ["1", "2", "3", "4"] },
   ],
   createdat: "date",
 };
@@ -17,6 +22,32 @@ const surveyforexapmle = {
 export default function AnswerSurveyT() {
   const [qcounter, setqcounter] = useState<number>(1);
 
+  const fristqHandler = () => {
+    setqcounter(1);
+  };
+  const lastqHandler = () => {
+    setqcounter(surveyforexapmle.questions.length);
+  };
+  const nextqHandler = () => {
+    if (qcounter < surveyforexapmle.questions.length) {
+      setqcounter((per) => per + 1);
+    } else {
+      return;
+    }
+  };
+  const previousqHandler = () => {
+    if (qcounter > 1) {
+      setqcounter((per) => per - 1);
+    } else {
+      return;
+    }
+  };
+  const buttons = [
+    {id:"1",name:"اولین",func:fristqHandler},
+    {id:"2",name:"قبلی",func:previousqHandler},
+    {id:"3",name:"بعدی",func:nextqHandler},
+    {id:"4",name:"اخرین",func:lastqHandler},
+  ]
   return (
     <div>
       <div>
@@ -25,15 +56,19 @@ export default function AnswerSurveyT() {
 
       <div>
         <div>
-            <p>{qcounter}</p>
-            <p>{surveyforexapmle.questions[qcounter-1].title}</p>
+          <p>{qcounter}</p>
+          <p>{surveyforexapmle.questions[qcounter - 1].title}</p>
         </div>
-        
+        <div></div>
+
         <div>
-          <button>first</button>
-          <button>previous</button>
-          <button>next</button>
-          <button>last</button>
+          {buttons.map(item=>(
+            <button key={item.id} onClick={item.func} className="p-5">{item.name}</button>
+          ))}
+          {/* <button onClick={fristqHandler}>first</button>
+          <button onClick={previousqHandler}>previous</button>
+          <button onClick={nextqHandler}>next</button>
+          <button onClick={lastqHandler}>last</button> */}
         </div>
       </div>
     </div>
